a383ac7adda7bcf330a9d770d5c85630f82cb0d7
Trevor Snell <trevorsnell@optonline.net>
2022-03-22 14:58:09 -0400

    first

---
+++ assets/quiz.js
@@ -0,0 +1,21 @@
+// Assignment code here
+
+function length(){
+  var passLength= prompt("Enter length of password from 8 - 128 characters.");
+  alert (passLength);
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
